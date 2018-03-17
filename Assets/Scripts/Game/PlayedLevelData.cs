46f122aaeca7125dd4a4a4a9307b93aa21c09ff6
David Eastman <davidjeastman@gmail.com>
2018-03-17 09:54:06 -0400

    Add end of level screen

---
+++ Assets/Scripts/Game/PlayedLevelData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayedLevelData{
+
+    public float overallScore;
+    public int notesHit;
+    public int totalNumberNotes;
+    public float percentage;
+    public float noteStreak;
+
+}
