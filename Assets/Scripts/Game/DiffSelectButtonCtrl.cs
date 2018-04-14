dd1d78eaf45048f04b435f84c659fa649887c8fb
David Eastman <davidjeastman@gmail.com>
2018-04-14 08:48:10 -0400

    Add sounds to menu

--- Assets/Scripts/Game/DiffSelectButtonCtrl.cs
+++ Assets/Scripts/Game/DiffSelectButtonCtrl.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DiffSelectButtonCtrl : MonoBehaviour {
-	
-	MenuController menuController;
+    
+    MenuController menuController;
 
-	public Difficulty difficulty;
+    public Difficulty difficulty;
 
-	private void Awake()
-	{
-		menuController = GameObject.FindGameObjectWithTag("MenuController").GetComponent<MenuController>();
-	}
-	public void SelectDifficulty()
-	{
-		menuController.SelectDifficulty(difficulty);
-	}
+    public AudioSource _buttonSound;
+    public AudioClip _buttonHover;
+
+    private void Awake()
+    {
+        menuController = GameObject.FindGameObjectWithTag("MenuController").GetComponent<MenuController>();
+    }
+    public void SelectDifficulty()
+    {
+        menuController.SelectDifficulty(difficulty);
+    }
+
+    public void HoverSound()
+    {
+        _buttonSound.PlayOneShot(_buttonHover);
+    }
+
+
 }
--- Assets/Scripts/Game/SongSelectButtonCtrl.cs
+++ Assets/Scripts/Game/SongSelectButtonCtrl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SongSelectButtonCtrl : MonoBehaviour {
 
-	MenuController menuController;
-	public int id;
+    MenuController menuController;
+    public int id;
 
-	private void Awake()
-	{
-		menuController = GameObject.FindGameObjectWithTag("MenuController").GetComponent<MenuController>();
-	}
-	public void SelectSong()
-	{
-		menuController.SelectSong(id);
-	}
+    public AudioSource _buttonSound;
+    public AudioClip _buttonHover;
+
+    private void Awake()
+    {
+        menuController = GameObject.FindGameObjectWithTag("MenuController").GetComponent<MenuController>();
+    }
+    public void SelectSong()
+    {
+        menuController.SelectSong(id);
+    }
+
+    public void HoverSound()
+    {
+        _buttonSound.PlayOneShot(_buttonHover);
+    }
 }
